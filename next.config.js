c280d50126a1c20770840e0ae6eae3527b34b43d
arzzzzi <kantyshevmagomed@gmail.com>
2023-01-03 16:53:12 +0300

    Header done

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        domains: ['links.papareact.com'],
+    }
 }
-
-module.exports = nextConfig
